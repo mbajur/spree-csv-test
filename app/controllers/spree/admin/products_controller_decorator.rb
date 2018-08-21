ece0b1fdd2779ba834c7f2267f9c6f62d7f0e131
mbajur <mbajur@gmail.com>
2018-08-21 10:55:52 +0200

    First fully working version of ProductsCsvImporter class

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,4 +1,30 @@
+require 'csv'
+
 Spree::Admin::ProductsController.class_eval do
   def import
+    @products_import_form = Products::ImportForm.new
+  end
+
+  def import_create
+    @products_import_form = Products::ImportForm.new(products_import_params)
+
+    if @products_import_form.valid?
+      result = @products_import_form.save
+
+      @created_products  = result.created_products
+      @existing_products = result.existing_products
+      @invalid_products  = result.invalid_products
+      @invalid_rows      = result.invalid_rows
+
+      render :import_results
+    else
+      render :import
+    end
+  end
+
+  private
+
+  def products_import_params
+    params.require(:products_import).permit(:file, :taxonomy_id, :shipping_category_id)
   end
 end
--- config/initializers/spree_routes.rb
+++ config/initializers/spree_routes.rb
@@ -1,5 +1,6 @@
 Spree::Core::Engine.routes.prepend do
   namespace :admin do
     get "products/import", controller: :products, action: :import
+    post "products/import", controller: :products, action: :import_create
   end
 end
